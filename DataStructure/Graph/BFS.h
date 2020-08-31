4266af80c459f27c4bf8c6d97f60f30635b1f539
eunjune <iws1605@naver.com>
2020-09-01 00:23:44 +0900

    BFS

---
+++ DataStructure/Graph/BFS.h
@@ -0,0 +1,26 @@
+#ifndef AL_GRAPH_BFS
+#define AL_GRAPH_BFS
+
+#include "../LinkedList/DoubleLinkedList.h"
+
+enum {A,B,C,D,E,F,G,H,I,J};
+
+typedef struct _ual
+{
+	int numV;
+	int numE;
+	List* adjList;
+	int* visitInfo;
+} ALGraph;
+
+void GraphInit(ALGraph* pg, int nv);
+
+void GraphDestroy(ALGraph* pg);
+
+void AddEdge(ALGraph* pg, int fromV, int toV);
+
+void ShowGraphEdgeInfo(ALGraph* pg);
+
+void BFShowGraphVertex(ALGraph* pg, int startV);
+
+#endif // !AL_GRAPH_BFS
